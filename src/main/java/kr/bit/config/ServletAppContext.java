11b8a3fcfb7cc41e769faf255446e627c1703e65
김태현 <kokiyo1030@naver.com>
2025-01-10 14:48:14 +0900

    list 구현들

--- src/main/java/kr/bit/config/ServletAppContext.java
+++ src/main/java/kr/bit/config/ServletAppContext.java
@@ -1,14 +1,66 @@
 package kr.bit.config;
 
-import org.springframework.context.annotation.Configuration;
+import kr.bit.bean.Student;
+import kr.bit.mapper.SubjectMapper;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.annotation.Resource;
+
 @Configuration
 @EnableWebMvc
+@ComponentScan(basePackages = {"kr.bit.controller", "kr.bit.dao", "kr.bit.service"})
+@PropertySource("/WEB-INF/properties/db.properties")
 public class ServletAppContext implements WebMvcConfigurer {
+
+    @Value("${db.classname}")
+    private String db_classname;
+
+    @Value("${db.url}")
+    private String db_url;
+
+    @Value("${db.username}")
+    private String db_username;
+
+    @Value("${db.password}")
+    private String db_password;
+
+    @Resource(name = "loginSession")
+    private Student loginSession;
+
+    @Bean
+    public BasicDataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(db_classname);
+        dataSource.setUrl(db_url);
+        dataSource.setUsername(db_username);
+        dataSource.setPassword(db_password);
+        return dataSource;
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(BasicDataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+
+        SqlSessionFactory sessionFactory = sqlSessionFactoryBean.getObject();
+        return sessionFactory;
+    }
+
+    @Bean
+    public MapperFactoryBean<SubjectMapper> subjectMapper(SqlSessionFactory sqlSessionFactory) {
+        MapperFactoryBean<SubjectMapper> mapperFactoryBean = new MapperFactoryBean<>(SubjectMapper.class);
+        mapperFactoryBean.setSqlSessionFactory(sqlSessionFactory);
+        return mapperFactoryBean;
+    }
 
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
--- src/main/java/kr/bit/mapper/SubjectMapper.java
+++ src/main/java/kr/bit/mapper/SubjectMapper.java
@@ -1,4 +1,18 @@
 package kr.bit.mapper;
 
+import kr.bit.bean.Subject;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
 public interface SubjectMapper {
+
+    @Select("select * from subject_table")
+    List<Subject> findAllSubject();
+
+    @Select("select distinct sub.subject_name " +
+            "from student_table stu, subject_table sub, enrolled_subject_table enr " +
+            "where enr.subject_num=sub.subject_num " +
+            "and enr.student_num=#{student_num};")
+    List<Subject> findAllSubjectByUserId(int student_num);
 }
